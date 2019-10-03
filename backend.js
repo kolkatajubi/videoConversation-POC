0404422329ced49c871518d7a38e90ff5457c89b
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-03 17:41:13 +0530

    host v1.0

---
+++ backend.js
@@ -0,0 +1,104 @@
+// Dependencies
+const express = require("express");
+const app = express();
+
+const bodyparser = require("body-parser");
+
+app.use(
+  bodyparser.urlencoded({
+    extended: false
+  })
+);
+
+// Defining IP-Address and PORT number
+const ipaddress = "127.0.0.1";
+const port = 2020;
+
+// Listening to the IP-Address:PORT number
+app.listen(port, ipaddress, () =>
+  console.log(`Listening at ${ipaddress}:${port}...`)
+);
+
+// Body Parser will parse the HTML and return it in JSON format
+app.use(bodyparser.json());
+
+app.post("/map", (req, res) => {
+  // Reading the excel file and creating JSON Objects
+  console.log("JSON DATA");
+  res.json({
+    status: "success",
+    nextMapData: nextMapData,
+    urlMapData: urlMapData
+  });
+});
+// app.get("/", (req, res) => {
+//   // Reading the excel file and creating JSON Objects
+// });
+
+// ============================================================================= //
+
+const xl = require("xlsx");
+const book = xl.readFile("test.xlsx");
+const sheetName = book.SheetNames;
+const nextMap = sheetName[0];
+const urlMap = sheetName[1];
+
+// Reading the excel file and creating JSON Objects
+var nextMapData = xl.utils.sheet_to_json(book.Sheets[nextMap]);
+var urlMapData = xl.utils.sheet_to_json(book.Sheets[urlMap]);
+
+console.log(nextMapData);
+console.log("===========================================================");
+console.log(urlMapData);
+
+// --------------------------------------------------------------------------------------
+//                            HOSTING FILES
+// --------------------------------------------------------------------------------------
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/videoxlsx.html");
+});
+app.get("/videofeed", (req, res) => {
+  res.sendFile(__dirname + "/videofeed.js");
+});
+app.get("/css", (req, res) => {
+  res.sendFile(__dirname + "/stylexls.css");
+});
+app.get("/start", (req, res) => {
+  res.sendFile(__dirname + "/chunu.mp4");
+});
+app.get("/a", (req, res) => {
+  res.sendFile(__dirname + "/A10.mp4");
+});
+app.get("/b", (req, res) => {
+  res.sendFile(__dirname + "/B20.mp4");
+});
+app.get("/C", (req, res) => {
+  res.sendFile(__dirname + "/C10.mp4");
+});
+app.get("/x", (req, res) => {
+  res.sendFile(__dirname + "/E1.mp4");
+});
+app.get("/y", (req, res) => {
+  res.sendFile(__dirname + "/E2.mp4");
+});
+app.get("/z", (req, res) => {
+  res.sendFile(__dirname + "/E3.mp4");
+});
+app.get("/p", (req, res) => {
+  res.sendFile(__dirname + "/E4.mp4");
+});
+app.get("/q", (req, res) => {
+  res.sendFile(__dirname + "/E5.mp4");
+});
+app.get("/r", (req, res) => {
+  res.sendFile(__dirname + "/E6.mp4");
+});
+app.get("/s", (req, res) => {
+  res.sendFile(__dirname + "/chunu.mp4");
+});
+app.get("/ty", (req, res) => {
+  res.sendFile(__dirname + "/TY.mp4");
+});
+// --------------------------------------------------------------------------------------
+//                            END OF HOSTING
+// --------------------------------------------------------------------------------------
--- videofeed.js
+++ videofeed.js
@@ -1,66 +1,93 @@
-var nextMapData = [
-  { Current: "start", Next: "A, B" },
-  { Current: "A", Next: "X, Y, Z" },
-  { Current: "B", Next: "A, P, Q" },
-  { Current: "X", Next: "P, Q, R, S" },
-  { Current: "Y", Next: "A, X" },
-  { Current: "Z", Next: "TY" },
-  { Current: "P", Next: "TY" },
-  { Current: "Q", Next: "TY" },
-  { Current: "R", Next: "TY" },
-  { Current: "S", Next: "TY" }
-];
+// var nextMapData = [
+//   { Current: "start", Next: "A, B" },
+//   { Current: "A", Next: "X, Y, Z" },
+//   { Current: "B", Next: "A, P, Q" },
+//   { Current: "X", Next: "P, Q, R, S" },
+//   { Current: "Y", Next: "A, X" },
+//   { Current: "Z", Next: "TY" },
+//   { Current: "P", Next: "TY" },
+//   { Current: "Q", Next: "TY" },
+//   { Current: "R", Next: "TY" },
+//   { Current: "S", Next: "TY" }
+// ];
 
-var urlMapData = [
-  { Nodes: "start", URL: "chunu.mp4" },
-  { Nodes: "A", URL: "A10.mp4" },
-  { Nodes: "B", URL: "B20.mp4" },
-  { Nodes: "C", URL: "C10.mp4" },
-  { Nodes: "X", URL: "E1.mp4" },
-  { Nodes: "Y", URL: "E2.mp4" },
-  { Nodes: "Z", URL: "E3.mp4" },
-  { Nodes: "P", URL: "E4.mp4" },
-  { Nodes: "Q", URL: "E5.mp4" },
-  { Nodes: "R", URL: "E6.mp4" },
-  { Nodes: "S", URL: "chunu.mp4" },
-  { Nodes: "TY", URL: "TY.mp4" }
-];
+// var urlMapData = [
+//   { Nodes: "start", URL: "chunu.mp4" },
+//   { Nodes: "A", URL: "A10.mp4" },
+//   { Nodes: "B", URL: "B20.mp4" },
+//   { Nodes: "C", URL: "C10.mp4" },
+//   { Nodes: "X", URL: "E1.mp4" },
+//   { Nodes: "Y", URL: "E2.mp4" },
+//   { Nodes: "Z", URL: "E3.mp4" },
+//   { Nodes: "P", URL: "E4.mp4" },
+//   { Nodes: "Q", URL: "E5.mp4" },
+//   { Nodes: "R", URL: "E6.mp4" },
+//   { Nodes: "S", URL: "chunu.mp4" },
+//   { Nodes: "TY", URL: "TY.mp4" }
+// ];
 
-var videoID = "";
+var nextMapData;
+var urlMapData;
+
+var videoID = "start";
 var currentNextNodes = [];
 var currentNextURLs = [];
+var status = 0;
+
+$(document).ready(() => {
+  $.ajax({
+    url: "http://127.0.0.1:2020/map",
+    type: "post",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    success: resp => {
+      console.log("Request server resolved.....");
+      nextMapData = data.nextMapData;
+      urlMapData = data.urlMapData;
+    },
+    error: err => {
+      console.log("Error");
+    }
+  });
+});
 
 (() => {
   setInterval(() => {
+    console.log(videoID);
     var videoDuration = document.getElementById("myVideo").duration.toFixed(2);
     var videoTime = document.getElementById("myVideo").currentTime.toFixed(2);
     // console.log(videoTime);
-    if (videoTime == videoDuration - 2.0) {
-      console.log("2secs left...");
-      createButton();
-    }
-  }, 10);
+    if (status == 0)
+      if (videoTime == videoDuration - 2.0) {
+        status = 1;
+        console.log("2secs left...");
+        createButton();
+      }
+  }, 5);
 })();
 
 function createButton() {
   console.log("create button called...");
-  videoID = $("video#myVideo source").attr("id");
+  // videoID = $("video#myVideo source").attr("id");
   // var id = $(this).attr("id");
   console.log("Video ID : ", videoID);
   getURLs(videoID);
   console.log("next nodes ....", currentNextNodes);
   console.log("next urls ....", currentNextURLs);
-  for (var i in currentNextNodes) {
-    $(".chat").append(
-      "<button id=" +
-        currentNextNodes[i] +
-        " onclick='changeSource(" +
-        i +
-        ");'>" +
-        currentNextNodes[i] +
-        "</button>"
-    );
-  }
+  if (currentNextNodes.length > 1)
+    for (var i in currentNextNodes) {
+      $(".chat").append(
+        "<button id=" +
+          currentNextNodes[i] +
+          " onclick='changeSource(" +
+          i +
+          ");'>" +
+          currentNextNodes[i] +
+          "</button>"
+      );
+    }
+  else changeSource(0);
 }
 
 function getURLs(current) {
@@ -68,7 +95,10 @@ function getURLs(current) {
   currentNextNodes = [];
   for (i in nextMapData) {
     if (nextMapData[i].Current === current) {
-      currentNextNodes = nextMapData[i].Next.split(", ");
+      currentNextNodes = nextMapData[i].Next.split(",");
+      for (t in currentNextNodes) {
+        currentNextNodes[t] = currentNextNodes[t].trim();
+      }
       console.log(currentNextNodes);
       currentNextURLs = [];
       for (i in currentNextNodes) {
@@ -99,4 +129,7 @@ function changeSource(index) {
   // video.src = currentNextURLs[index];
   video.load();
   video.play();
+  console.log("Changesource completed");
+  videoID = $("video#myVideo source").attr("id");
+  status = 0;
 }
